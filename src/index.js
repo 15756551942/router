0032661ccf1c01a615efe43aa62409b6189a37a5
15756551942 <1924730917@qq.com>
2020-10-11 23:06:17 +0800

    third

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import BackgroundPage from './components/app/backgroundpage'
+import {BrowserRouter} from 'react-router-dom'
+
+import App from './components/app/app'
 import './index.css'
-ReactDOM.render(<BackgroundPage/>,document.getElementById('root'))
+
+ReactDOM.render(
+    (   
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    ),
+    document.getElementById('root')
+)
